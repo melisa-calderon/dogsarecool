3c4bd80d1f8ee7b8a7fc6c808fe053bf1880bb9d
Melisa Calderon <mecalderon@vassar.edu>
2021-07-13 16:44:00 +0000

    added captions

--- main_file.py
+++ main_file.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm
 from flask_sqlalchemy import SQLAlchemy
-
+from audio import printWAV
+import time, random, threading
+from turbo_flask import Turbo
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '9ec7f20aec87d783c5f9aa84f11d7c7a'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+
+interval= 9
+FILE_NAME = "The_Science_of_DOGS.wav"
+turbo = Turbo(app)
 
 class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
@@ -47,8 +53,45 @@ def register():
 @app.route("/captions")
 def captions():
     TITLE = "The Science of DOGS"
-    FILE_NAME = "The_Science_of_DOGS.wav"
     return render_template('captions.html', songName=TITLE, file=FILE_NAME)
-  
+
+
+@app.before_first_request
+def before_first_request():
+    #resetting time stamp file to 0
+    file = open("pos.txt","w") 
+    file.write(str(0))
+    file.close()
+
+    #starting thread that will time updates
+    threading.Thread(target=update_captions).start()
+
+
+@app.context_processor
+def inject_load():
+    # getting previous time stamp
+    file = open("pos.txt","r")
+    pos = int(file.read())
+    file.close()
+
+    # writing next time stamp
+    file = open("pos.txt","w")
+    file.write(str(pos+interval))
+    file.close()
+
+    #returning captions
+    return {'caption':printWAV(FILE_NAME, pos=pos, clip=interval)}
+
+
+def update_captions():
+    with app.app_context():
+        while True:
+            # timing thread waiting for the interval
+            time.sleep(interval)
+
+            # forcefully updating captionsPane with caption
+            turbo.push(turbo.replace(render_template('captionsPane.html'), 'load'))
+
+
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
