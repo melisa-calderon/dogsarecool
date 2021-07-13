b35966615bc83445ec7b07c412e4544c4c6d302e
Melisa Calderon <mecalderon@vassar.edu>
2021-07-13 15:47:04 +0000

    saving changes

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -13,7 +13,12 @@ class BasicTests(unittest.TestCase):
     #### tests ####
     ###############
 
-    def test_test(self):
+    def test_main_page(self):
+        response = self.app.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_main_page(self):
         response = self.app.get('/', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
 
